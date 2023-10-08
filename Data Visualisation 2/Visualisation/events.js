bd175765e0829c79e0b34bf497ef1933c8b9d4dd
Jngg0093 <142304714+Jngg0093@users.noreply.github.com>
2023-10-08 23:17:20 +1100

    Add files via upload

--- Data Visualisation 2/Visualisation/events.js
+++ Data Visualisation 2/Visualisation/events.js
@@ -26,7 +26,25 @@ function viewCountry() {
     chartImage3.style.display = "block"
 }
 
+function viewAll() {
+    var chartImage1 = document.getElementById("nukeoverall")
+    var chartImage2 = document.getElementById("nukeyear");
+    chartImage1.style.display = "block"
+    chartImage2.style.display = "none"
+}
+
+function viewYearly() {
+    var chartImage1 = document.getElementById("nukeoverall")
+    var chartImage2 = document.getElementById("nukeyear");
+    chartImage1.style.display = "none"
+    chartImage2.style.display = "block"
+}
+
+
+
 // Script calls
 document.getElementById('OverviewButton').addEventListener('click', viewOverview)
 document.getElementById('AllianceButton').addEventListener('click', viewAlliance)
 document.getElementById('PerCountryButton').addEventListener('click', viewCountry)
+document.getElementById('AllButton').addEventListener('click', viewAll)
+document.getElementById('YearlyButton').addEventListener('click', viewYearly)
